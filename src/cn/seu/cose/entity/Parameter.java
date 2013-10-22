985f3af16644f2126a6a94d0ced949973cfdcdcf
bingluo <bingluo@foxmail.com>
2013-10-22 16:57:03 +0800

    parameter ibatis_sqlmap

---
+++ src/cn/seu/cose/entity/Parameter.java
@@ -0,0 +1,40 @@
+package cn.seu.cose.entity;
+
+public class Parameter {
+	private String parameterKey;
+	private String parameterName;
+	private String parameterValue;
+	private String extra;
+
+	public String getParameterKey() {
+		return parameterKey;
+	}
+
+	public void setParameterKey(String parameterKey) {
+		this.parameterKey = parameterKey;
+	}
+
+	public String getParameterName() {
+		return parameterName;
+	}
+
+	public void setParameterName(String parameterName) {
+		this.parameterName = parameterName;
+	}
+
+	public String getParameterValue() {
+		return parameterValue;
+	}
+
+	public void setParameterValue(String parameterValue) {
+		this.parameterValue = parameterValue;
+	}
+
+	public String getExtra() {
+		return extra;
+	}
+
+	public void setExtra(String extra) {
+		this.extra = extra;
+	}
+}
