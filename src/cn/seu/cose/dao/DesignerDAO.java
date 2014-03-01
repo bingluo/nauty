77e8af32255fcd6240150d3097571ef22bba28c8
bingluo <bingluo@foxmail.com>
2014-03-01 21:21:52 +0800

    designer dao

---
+++ src/cn/seu/cose/dao/DesignerDAO.java
@@ -0,0 +1,21 @@
+package cn.seu.cose.dao;
+
+import java.util.List;
+
+import cn.seu.cose.entity.Designer;
+
+public interface DesignerDAO {
+	List<Designer> getAllDesigners();
+
+	List<Designer> getAllCertificatedDesigners();
+
+	Designer getDesignerById(int id);
+
+	Designer getDesignerByUsernameAndPswd(String username, String pswd);
+
+	void insertDesigner(Designer designer);
+
+	void certificateDesignerById(int id);
+
+	void updateDesigner(Designer designer);
+}
