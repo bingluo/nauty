2cc8b79ebb6f9d45fc22166aa11f3cc33eb50d82
bingluo <bingluo@foxmail.com>
2014-03-02 21:19:21 +0800

    activity news dao

--- src/cn/seu/cose/dao/ActivityApplicationDAOImpl.java
+++ src/cn/seu/cose/dao/ActivityApplicationDAOImpl.java
@@ -6,11 +6,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+import org.springframework.stereotype.Component;
 
 import cn.seu.cose.entity.ActivityApplication;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 
+@Component
 @SuppressWarnings("unchecked")
 public class ActivityApplicationDAOImpl extends SqlMapClientDaoSupport
 		implements ActivityApplicationDAO {
@@ -43,6 +45,6 @@ public class ActivityApplicationDAOImpl extends SqlMapClientDaoSupport
 		map.put("userId", userId);
 		map.put("activityId", activityId);
 		getSqlMapClientTemplate().delete(
-				"ACTIVITY_APPLICATION.deleteActivityById", map);
+				"ACTIVITY_APPLICATION.deleteActivityApplication", map);
 	}
 }
---
+++ src/cn/seu/cose/dao/ActivityNewsDAO.java
@@ -0,0 +1,15 @@
+package cn.seu.cose.dao;
+
+import java.util.List;
+
+import cn.seu.cose.entity.ActivityNews;
+
+public interface ActivityNewsDAO {
+	List<ActivityNews> getActivityNewsByActivityId(int id);
+
+	void insertActivityNews(ActivityNews activityNews);
+
+	void deleteActivityNews(int id);
+
+	void updateActivityNews(ActivityNews activityNews);
+}
