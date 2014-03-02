b0e45675ee0790458f1ba58782cb390f0e1ecedb
bingluo <bingluo@foxmail.com>
2014-03-02 19:51:40 +0800

    activity application dao

---
+++ src/cn/seu/cose/dao/ActivityApplicationDAO.java
@@ -0,0 +1,13 @@
+package cn.seu.cose.dao;
+
+import java.util.List;
+
+import cn.seu.cose.entity.ActivityApplication;
+
+public interface ActivityApplicationDAO {
+	List<ActivityApplication> getActivityApplicationsByActivityId(int id);
+
+	void insertActivityApplication(int userId, int activityId);
+
+	void deleteActivityApplication(int userId, int activityId);
+}
---
+++ src/cn/seu/cose/dao/ActivityApplicationDAOImpl.java
@@ -0,0 +1,48 @@
+package cn.seu.cose.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+import cn.seu.cose.entity.ActivityApplication;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+@SuppressWarnings("unchecked")
+public class ActivityApplicationDAOImpl extends SqlMapClientDaoSupport
+		implements ActivityApplicationDAO {
+
+	@Autowired(required = true)
+	public void setSqlMapClientTemp(SqlMapClient sqlMapClient) {
+		setSqlMapClient(sqlMapClient);
+	}
+
+	@Override
+	public List<ActivityApplication> getActivityApplicationsByActivityId(int id) {
+		return getSqlMapClientTemplate()
+				.queryForList(
+						"ACTIVITY_APPLICATION.selectActivityEnrollmentByActivityId",
+						id);
+	}
+
+	@Override
+	public void insertActivityApplication(int userId, int activityId) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("userId", userId);
+		map.put("activityId", activityId);
+		getSqlMapClientTemplate().insert(
+				"ACTIVITY_APPLICATION.insertActivityApplication", map);
+	}
+
+	@Override
+	public void deleteActivityApplication(int userId, int activityId) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("userId", userId);
+		map.put("activityId", activityId);
+		getSqlMapClientTemplate().delete(
+				"ACTIVITY_APPLICATION.deleteActivityById", map);
+	}
+}
