1422b0466e346aca398e9507833a83fa1a51faf1
bingluo <bingluo@foxmail.com>
2014-05-27 00:30:33 +0800

    SEO

--- src/cn/seu/cose/controller/IndexController.java
+++ src/cn/seu/cose/controller/IndexController.java
@@ -43,6 +43,9 @@ public class IndexController extends AbstractController {
 		model.addAttribute("news", news);
 		model.addAttribute("activities", activities);
 		model.addAttribute("works", works);
+		model.addAttribute(
+				"meta",
+				"<meta name='Keywords' content='设计,工业设计,设计师,设计作品,工业设计协会,设计新闻,设计赛事,设计活动'/>  <meta name='Description' content='长三角（嘉兴）工业设计致力于工业设计发展，为优秀设计师与设计公司提供机遇。'/>");
 		basicIssue(model);
 		return "index";
 	}
